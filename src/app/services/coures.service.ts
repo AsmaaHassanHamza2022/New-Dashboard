6bba980ccfa6115c3d644edfcb42a35100a005c1
Asmaa Hassan Hamza <Eng.asmaahassanhamza98@gmail.com>
2022-12-17 17:45:57 +0200

    connect my app With firebase &get courses data

---
+++ src/app/services/coures.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { collectionData, Firestore,collection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { MyCourses } from '../models/Course';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CouresService {
+
+  constructor(private fire: Firestore) {
+   }
+
+  public getAllCourses(){
+   let item$: Observable<any>;
+
+        const collections = collection(this.fire, 'Courses');
+        item$ = collectionData(collections);
+
+        return item$;
+      
+  }
+}
