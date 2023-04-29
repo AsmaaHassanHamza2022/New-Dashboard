e221fbb0aed86e7bbda1cd551bf3e59d1e7096de
Asmaa Hassan Hamza <Eng.asmaahassanhamza98@gmail.com>
2023-04-29 17:34:38 +0300

    close dash board

--- src/app/pages/setting-page/setting-page.component.ts
+++ src/app/pages/setting-page/setting-page.component.ts
@@ -1,15 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-setting-page',
   templateUrl: './setting-page.component.html',
   styleUrls: ['./setting-page.component.scss']
 })
-export class SettingPageComponent implements OnInit {
+export class SettingPageComponent implements OnInit ,AfterViewInit{
 
-  constructor() { }
+  public types:any[]=[
+    {
+      name:"ASmaa",
+      type:"Female",
+      Age:30
+    },
+    {
+      name:"ASmaa",
+      type:"Female",
+      Age:30
+    },
+    {
+      name:"ASmaa",
+      type:"Female",
+      Age:30
+    },
+    {
+      name:"ASmaa",
+      type:"Female",
+      age:30
+    },
+    {
+      name:"ASmaa",
+      type:"Female",
+      Age:30
+    }
+  ]
+
+  constructor() { 
+    
+  }
+  ngAfterViewInit(): void {
+    let temp='';
+    for(let i=0 ; i >this.types.length ;i++){
+      temp+= `<div>{{x.`+this.types[i]+`}}</div>
+      <div>{{x.`+this.types[i]+`}}</div>
+      <div>{{x.`+this.types[i]+`}}</div>
+      <hr>`
+    }
+    document.getElementsByClassName('bodyrr')[0].innerHTML=temp;
+  }
 
   ngOnInit(): void {
+    let temp='';
+    for(let i=0 ; i >this.types.length ;i++){
+
+      temp+= `<div>{{x.`+this.types[i]+`}}</div>
+      <div>{{x.`+this.types[i]+`}}</div>
+      <div>{{x.`+this.types[i]+`}}</div>
+      <hr>`
+
+    }
+
+    document.getElementsByClassName('body')[0].innerHTML=temp;
   }
 
 }
